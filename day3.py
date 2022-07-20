4a7fcdc73e0da538abbb7af5406c6d4dd82a8112
mohammedarbaz119 <mohammedarbazq16@gmail.com>
2022-07-21 00:05:38 +0530

    python changes

---
+++ day3.py
@@ -0,0 +1,20 @@
+age = 30
+height = 5.9
+print(age, height, sep=" ")
+complex = 1+3j
+print(complex)
+# # base = int(input("enter base: "))
+# # heightoftriangle = int(input("enter height : "))
+# areoftri = 1/2*base*heightoftriangle
+# print("area of triagnle: ", areoftri)
+
+
+# def per():
+#   a = int(input("enter side: "))
+# b = int(input("enter side: "))
+# c = int(input("enter side: "))
+# peri = (a+b+c)
+# print(f"perimmeter is {peri}")
+
+
+# per()
