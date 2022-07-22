0ce660a43fcd491c45e53066912ccbbba394f4bc
mohammedarbaz119 <mohammedarbazq16@gmail.com>
2022-07-23 01:00:07 +0530

    day 5 added

--- day3.py
+++ day3.py
@@ -18,3 +18,30 @@ print(complex)
 
 
 # per()
+a = "30"
+b = "days"
+c = "of"
+d = "python"
+e = a+b+c+d
+print(e)
+company = "coding for all"
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+s = company.find(" ")
+print(company.find("coding") != -1)
+print(company.replace("coding", "python"))
+print(company.split(" "))
+ad = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(',')
+print(ad)
+print("coding for all"[-1])
+print("Coding with all".startswith("Coding"))
+print("  coding for all  ".strip())
+print(' '.join(['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']))
+print("new char \n char")
+print(f"radius = {10}")
+print(f"area={3.14} * radius ** {2}")
+print(f"the are of circle with radius {10} is {314}")
---
+++ day5.py
@@ -0,0 +1,20 @@
+# l = []
+# nl = list()
+# list of fruits
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage',
+              'Onion', 'Carrot']      # list of vegetables
+animal_products = ['milk', 'meat', 'butter',
+                   'yoghurt']             # list of animal products
+web_techs = ['HTML', 'CSS', 'JS', 'React', 'Redux',
+             'Node', 'MongDB']  # list of web technologies
+countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+# list containing different data types
+lst = ['Asabeneh', 250, True, {'country': 'Finland', 'city': 'Helsinki'}]
+# challenges
+l = []
+l5 = [1, 2, 3, 4, 5]
+print(len(l5))
+print(f"{l5[0]} {l5[int((0+len(l5))/2)]} {l5[len(l5)-1]}")
+comapnies = ["Facebook", "Google", "Microsoft",
+             "Apple", "IBM", "Oracle", "Amazon"]
