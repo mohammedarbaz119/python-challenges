48aed735aa3a84803e82573d340f5d9355bfc472
mohammedarbaz119 <mohammedarbazq16@gmail.com>
2022-08-08 18:36:19 +0530

    new

--- day11.py
+++ day11.py
@@ -4,3 +4,47 @@
 
 
 # sum(10, 20)
+from audioop import reverse
+from cmath import pi
+
+
+# def area(r):
+#     return pi*r*r
+
+
+# print(area(10))
+
+
+# def sum(*a):
+#     s = 0
+#     if(all(type(x) == int for x in a)):
+#         print("cool")
+#     for x in a:
+#         if(type(x) != int):
+#             return "not cool"
+#         else:
+#             s += x
+
+#     return s
+
+
+# print(sum(1, 2, 3, 4))
+
+
+# def printl(l):
+#     for x in l:
+#         print(x)
+
+
+# printl([1, 23, 4])
+
+# def reversel(l):
+#     for x in range(len(l)-1, -1, -1):
+#         print(l[x])
+
+
+# reversel([1, 2, 32, 4])
+
+# def capitalize(l):
+#     for x in l:
+#         print(x.upper())
