b4f55b5a9272d6ddb5c90123e61c26eb9cd847f0
mohammedarbaz119 <mohammedarbazq16@gmail.com>
2022-08-09 20:32:27 +0530

    day 12

--- day11.py
+++ day11.py
@@ -6,6 +6,8 @@
 # sum(10, 20)
 from audioop import reverse
 from cmath import pi
+import math
+from queue import Empty
 
 
 # def area(r):
@@ -92,3 +94,74 @@ from cmath import pi
 
 
 # sumofeven(100)
+
+# level 2
+
+# def evenandodds(n):
+#     e = 0
+#     o = 0
+#     for i in range(0, n + 1):
+#         if(i % 2 == 0):
+#             e += 1
+#         else:
+#             o += 1
+#     print(f"no forr evens {e}")
+#     print(f"no of odds {o}")
+
+
+# evenandodds(1010)
+
+# def fact(n):
+#     s = 1
+#     for i in range(1, n+1):
+#         s *= i
+#     print(s)
+
+
+# fact(6)
+
+# def isempty(n):
+#     if(not n):
+#         return True
+#     else:
+#         return False
+
+
+# print(isempty([1, 2]))
+
+# def prime(n):
+#     for i in range(2, round(math.sqrt(n))):
+#         if(n % i == 0):
+#             return False
+#     return True
+
+
+# print(prime(22))
+
+# def isunique(n):
+#     return len(set(n)) == len(n)
+
+
+# print(isunique([1, 23, 3, 4, 4]))
+
+# def checktype(n, typee):
+#     return all(type(x) == typee for x in n)
+
+
+# print(checktype([1, 2, 3, 4], int))
+
+# def validvar(n):
+#     return n.isidentifier()
+
+
+# print(validvar("-truew"))
+import day10
+a = day10.country
+
+
+# def most10(a):
+#     a.sort(key=lambda a: a["population"], reverse=True)
+#     return a[0:10]
+
+
+# print(most10(a))
---
+++ dway12.py
@@ -0,0 +1,17 @@
+import math
+from random import random
+import string
+
+
+# def rand_id(n, c):
+#     a = string.ascii_letters+string.digits
+#     d = []
+
+#     for x in range(n+1):
+#         s = ""
+#         for i in range(c+1):
+#             s += a[math.floor(random()*len(a))]
+#         print(s)
+
+
+# rand_id(6, 16)
